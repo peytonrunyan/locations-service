a3006462def2a39ddfbce628989502b14641d306
Peyton Runyan <prunyan@recyclingpartnership.org>
2021-11-17 17:43:26 -0500

    setup for docker compose

--- cmd/geoservice/main.go
+++ cmd/geoservice/main.go
@@ -13,7 +13,7 @@ var (
 func main() {
 	fmt.Println("Starting server...")
 	fmt.Println("Processing GeoJSON files. This normally takes a bit.")
-	srv := server.NewHTTPServer(":" + port)
+	srv := server.NewHTTPServer("0.0.0.0:" + port)
 	fmt.Println("Processing complete.")
 	fmt.Printf("Server listening at port %s\n", port)
 	log.Fatal(srv.ListenAndServe())
